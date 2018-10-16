8b4d97aeb5ef62a20a9858e09758f511a16630e7
aLLeksey <aLLeksey@users.noreply.github.com>
2018-10-16 04:34:22 -0400

    lala

--- Chapter2.1/utmplib.c
+++ Chapter2.1/utmplib.c
@@ -6,10 +6,43 @@
 #define NRECS 16
 #define NULLUT ((struct utmp*)NULL)
 #define UTSIZE (sizeof(struct utmp))
+
+
 static char utmpbuf[NRECS * UTSIZE];
 static int num_recs;
 static int cur_rec;
 static int fd_utmp = -1;
+
+int utmp_seek(int record_offset, int base){
+  
+  switch(base){
+  case SEEK_SET:
+    if( record_offset < 0 )
+      return -1;
+    if( lseek(fd_utmp, record_offset * UTSIZE, SEEK_SET) == -1 ){
+      return -1;
+    }
+    cur_rec = num_recs = 0;
+    break;
+  case SEEK_CUR:
+    if ( lseek(fd_utmp, record_offset * UTSIZE, SEEK_CUR) == -1 ){
+      return -1;
+    }
+    cur_rec = num_recs = 0;
+  
+  case  SEEK_END:
+    if ( record_offset > 0){
+      return -1;
+    }
+    if (lseek(fd_utmp, record_offset * UTSIZE, SEEK_END) == -1 ){
+      return -1;
+    }
+    cur_rec = num_recs = 0;
+  default:
+    break;
+  }
+}
+
 utmp_open(char *filename){
   fd_utmp = open(filename, O_RDONLY);
   cur_rec = num_recs = 0;
@@ -39,4 +72,16 @@ int utmp_reload(){
 utmp_close(){
   if(fd_utmp != -1)
     close(fd_utmp);
+}
+// pos starts with zero
+int utmp_write()
+{
+  
+  size_t pos = cur_rec - 1;
+
+  if(lseek(fd_utmp, -(num_recs - pos) * UTSIZE, SEEK_CUR))
+    if(write(fd_utmp,  (struct utmp *) &utmpbuf[cur_rec * UTSIZE], UTSIZE) == UTSIZE)
+      if(lseek(fd_utmp, (num_recs - pos - 1)* UTSIZE  , SEEK_CUR))
+	return 0;
+  return 1;
 }
