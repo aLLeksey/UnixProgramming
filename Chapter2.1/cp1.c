b3e0f7c889ef17aa35295f1793ae964ebdd432ea
alex <aLLeksey@users.noreply.github.com>
2018-11-06 18:09:53 +0300

    cp1 done. Exsercises done.

--- Chapter2.1/cp1.c
+++ Chapter2.1/cp1.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFFERSIZE 4096
 #define COPYMODE 0644
@@ -10,15 +11,22 @@ void oops(char *,char *);
 int main(int ac, char *av[]){
   int in_fd, out_fd, n_chars;
   char buf[BUFFERSIZE];
-  if (ac != 3){
+  /*if (ac != 3){
     fprintf(stderr, "usage: %s source detination \n", * av);
     exit(1);
-  }
+    }*/
   if((in_fd = open(av[1], O_RDONLY)) == -1)
     oops("Cannt open", av[1]);
+
+  if (ac == 4 && strncmp(av[3],"-i",2) == 0){
+    printf ("A u shure?(y/n)\n");
+    char c = getchar();
+    if (c == 'n')
+      exit(0);
+  }
+  
   if ((out_fd = creat(av[2], COPYMODE)) == -1)
       oops("Cannot create", av[2]);
-
   //copy
 
   while((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0){
