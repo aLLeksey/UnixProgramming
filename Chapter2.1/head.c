6ff786ee31782dc6a25cf2116a2c30f8e0fcd659
aleksey <aLLeksey@users.noreply.github.com>
2019-01-25 00:34:18 +0300

    head in progress add stub options

--- Chapter2.1/head.c
+++ Chapter2.1/head.c
@@ -4,22 +4,39 @@
 int print_lines(const char* FILE_NAME, int cnt);
 int rev_print(FILE *f, int cnt);
 void print_c(char c, int times);
-
+void run(int first_file, int last_file,const char * argv[]);
 int main(int argc, char* argv[]){
 	if(argc <= 1){
-		return;
+		return 0;
 	}
-	for(int i=1; i < argc;i++){
-		print_lines(argv[i],10);
-		//print_c('#',80);
+	int oc;
+	char *bytes_arg;
+	char *lines_arg;
+		int q = 1;
+	while((oc = getopt(argc,argv,"cnqv")) != -1){
+		switch(oc){
+			case 'c':
+				bytes_arg=optarg;
+				break;
+			case 'n':
+				lines_arg=optarg;
+				break;
+			case 'q':
+				q = 0;
+				break;
+			case 'v':
+				q = 0;
+		}
 	}
-	
+	if(optind<argc){
+		run(optind,argc,argv);
+	}
 	return 0;
 }
 
 int print_lines(const char* FILE_NAME, int cnt){
 	FILE *f = fopen(FILE_NAME,"r");
-	if(f==0)return;
+	if(f==0)return 0;
 	printf("==> %s <==\n",FILE_NAME);
 	char a[1000];
 	for(int i = 0; i < cnt; i++){
@@ -35,4 +52,10 @@ void print_c(char c, int times){
 		i++;
 	}
 }	
-	
+
+void run(int first_file, int last_file,const char * argv[]){
+	for(int i = first_file; i < last_file;i++){
+		print_lines(argv[i],10);
+		//print_c('#',80);
+	}
+}
