f04ca427b5b0285bf651ad36411377ac2c5654cc
alexey <aLLeksey@users.noreply.github.com>
2021-01-30 01:02:39 +0300

    3:ls->sorted output SEGFAULT

--- Chapter3/ls2.c
+++ Chapter3/ls2.c
@@ -5,6 +5,8 @@
 #include<sys/stat.h>
 #include<unistd.h>
 
+char **a;
+
 void traverse(char *name);
 
 void ls_list(char *);
@@ -14,6 +16,13 @@ void show_file_info(char *, struct stat *);
 char *uid_to_name(uid_t);
 char *guid_to_name(gid_t);
 
+
+long long my_sort();
+int my_cmp(const void *a, const void *b);
+long long process_names();
+long long store(char* string);
+long long add_list(char* string);
+void list_free();
 
 void parse(int argc,char **argv);
 
@@ -36,14 +45,14 @@ int main(int argc, char ** argv){
   //optind == 1
   parse(argc,argv);
 
-  if(optind == argc){ //no OTHER options, current dir
+  if(optind == argc){ //no OTHER options=> current dir
     printf("optind=%d, argc=%d\n",optind,argc);
     traverse(".");
     return 0;
   }
   // else
   while(optind<argc){ 
-    //printf("%s\n",argv[optind++]);
+	    //printf("%s\n",argv[optind++]);
     traverse(argv[optind++]);
   }
 }
@@ -110,7 +119,7 @@ void parse(int argc, char **argv){
         process=ls_column;
         break;
       case 'l':
-        process=ls_list;
+        process=store;
         break;
     }
   }
@@ -146,7 +155,8 @@ void traverse(char *name){
     process(name);
     return;
   }
-    
+  
+  
   DIR *dir_ptr;
   struct dirent *direntptr;
   if((dir_ptr = opendir(name)) == NULL){
@@ -157,6 +167,14 @@ void traverse(char *name){
       process(direntptr -> d_name);
     }
     closedir(dir_ptr);
+  }
+//TODO
+//separete in 2(3) parts
+  if(process==store){
+    my_sort();
+    process_names();
+    free(a);
+    list_free();
   }
 }
 
@@ -184,10 +202,10 @@ void show_file_info(char *filename, struct stat *info_p){
   char modestr[11];
   mode_to_letters(info_p->st_mode,modestr);
   printf("%s", modestr);
-  printf("%4d ", (int)info_p->st_nlink);
-  printf("%-8s", uid_to_name(info_p->st_uid));
-  printf("%-8s", gid_to_name(info_p->st_gid));
-  printf("%8ld ", (long)info_p->st_size);
+  printf("%2d ", (int)info_p->st_nlink);
+  printf("%-4s", uid_to_name(info_p->st_uid));
+  printf("%-4s", gid_to_name(info_p->st_gid));
+  printf("%5ld ", (long)info_p->st_size);
   printf("%.12s", 4+ctime(&info_p->st_mtime));
   printf(" %s\n", filename);
 }
@@ -253,10 +271,91 @@ char *gid_to_name(gid_t gid){
   struct group *getgrgid(), *grp_ptr;
   static char numstr[10];
 
-  if((grp_ptr = getgrgid(gid)) == NULL){   //?????
+  if((grp_ptr = getgrgid(gid)) == NULL){   // if getgdgid(gid)==NULL
     snprintf(numstr,sizeof(numstr)/sizeof(numstr[0]),"% d", gid);
     return numstr;
   }
   else
     return grp_ptr->gr_name;
 }
+
+
+
+typedef struct list{
+  char *string;
+  struct list *next;
+}str_list;
+
+str_list *head=NULL;
+str_list *last=NULL;
+long long cnt=0;
+
+long long add_list(char *string){
+  if(last=NULL){
+    last=start=malloc(sizeof (str_list));
+    head->next=NULL;
+    head->string=string;
+  }
+  else{
+    last->next=malloc(sizeof (str_list));
+    last->next->next=NULL;
+    last->next->string=string;
+    last=last->next;
+  }
+  cnt++;
+  return 0;
+}
+
+long long store(char *string){
+  add_list(string);
+  return 0;
+}
+
+
+
+int my_cmp(const void* a, const void* b){
+  return strcmp(*(const char **)a, *(const char **)b);
+}
+
+long long my_sort(){
+  a=malloc(cnt * (sizeof (char*)));
+  bzero(a,cnt * sizeof(char*)); //??? Can we  guarantee that memory is conasequal and we not overwrite smth???
+  
+  str_list* strl=head;
+  for(int i = 0; i < cnt && strl !=NULL; i++, strl=strl->next){
+    a[i]=strl->string;
+  }
+  
+  qsort(a,cnt,sizeof(const char *),strcmp);
+  return 0;
+}
+
+
+long long process_names(){
+  my_sort();
+
+  for(int i = 0; i < cnt; i++){
+    ls_list(a[i]);
+  }
+  
+  return 0;
+}
+
+void list_free(){
+  
+  str_list *str_l,*next;
+  str_l=head;
+  while(str_l != NULL){
+    next=str_l->next;
+    free(str_l);
+    str_l=next;
+  }
+}
+
+
+
+
+
+
+
+
